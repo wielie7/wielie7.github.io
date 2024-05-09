681a4cc1dc231f1e1846d8a7144921853c5aa861
Wielan du Plessis <99413610+wielie7@users.noreply.github.com>
2024-05-09 09:28:16 +0200

    copyright canonical tag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
 	"use strict";
     
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : Khanas HTML Template
-* Version       : 1.0
-==================================== */
 
 
 
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. Smooth Scroll spy
-3. Progress-bar
-4. owl carousel
-5. welcome animation support
-======================================*/
 
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
@@ -55,21 +43,7 @@ $(document).ready(function(){
 			offset:0
 		});
 
-	// 3. Progress-bar
 	
-		var dataToggleTooTip = $('[data-toggle="tooltip"]');
-		var progressBar = $(".progress-bar");
-		if (progressBar.length) {
-			progressBar.appear(function () {
-				dataToggleTooTip.tooltip({
-					trigger: 'manual'
-				}).tooltip('show');
-				progressBar.each(function () {
-					var each_bar_width = $(this).attr('aria-valuenow');
-					$(this).width(each_bar_width + '%');
-				});
-			});
-		}
 	
 	// 4. owl carousel
 	
